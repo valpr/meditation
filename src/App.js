20bf691801cc490ab85180159a93e4fa1bee13c3
valpr <me@andrewfong.ca>
2020-03-15 22:25:51 -0400

    Created background, acquired SVGs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
+import {ReactComponent as Rain} from './rain.svg'
+import {ReactComponent as HG} from './hourglass.svg'
+import {ReactComponent as Radio} from './radio.svg'
+import TimeBar from './ProgressBar'
 
 function App() {
+  const [timer, setTimer] = useState(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Background">
+      <div className="Container">
+      <div className="SVGContainer">
+        <Rain height={30} width={30}  />
+
+      </div>
+        <div className="SVGContainer">
+        <Radio height={30} width={30}  />
+
+      </div>
+      <div className="SVGContainer">
+        <HG height={30} width={30}  />
+
+      </div>
+      <TimeBar/>
+
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/ProgressBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function TimeBar(props){
+    const [timeLeft, setTimeLeft] = useState(3600);
+    return(
+        <div id="bar">
+            <div id="fill">
+
+            </div>
+        </div>
+    )
+}
+
+export default TimeBar;
